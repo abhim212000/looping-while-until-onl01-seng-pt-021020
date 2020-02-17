8c7e74cd50cb9248a01701f427ce2971a1bff8fd
Abhimanue Manchanda <abhim212000@yahoo.co.in>
2020-02-17 01:09:11 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -2,7 +2,7 @@ def using_while
   using_while = "Wingardium Leviosa"
   levitation_force = 6
   while levitation_force < 10
-  puts "using_while"
+  puts "Wingardium Leviosa"
   levitation_force += 1
 end
 end
